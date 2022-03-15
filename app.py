2d772d15fcbab82af1d232e36a36d7809b2a03d3
IronK77 <53202149+IronK77@users.noreply.github.com>
2022-03-15 17:13:32 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,16 +4,11 @@ Created on Sun Mar 13 22:52:13 2022
 
 @author: Administrator
 """
-   
 
 import streamlit as st
-import numpy as np
 import pandas as pd
-from datetime import date
-import os
-
-
-today = date.today()
+import plotly.graph_objects as go
+from PIL import Image
 
 st.set_page_config(
     page_title="商道绿色债券查询",
@@ -62,17 +57,42 @@ with t1:
 with t2:
     st.write("")
     st.write("")
-    st.write("""
-    **商道融绿** | Syntao Green Finance
-    """)
+    image = Image.open('STGF_logo.png')
+    st.image(image,width=320)
+   #st.write("""
+   #**商道融绿** | Syntao Green Finance
+   #""")
 
 st.write("")
 st.markdown("***")
 length=""
 st.write(f"目前选择的产品/项目是 **{main_product}**")
-
-st.write('该项目共有',length,'条记录')
 st.markdown("***")
+std=products.L3[products["name_product"]== main_product]
+st.write("对应的目录条目为：")
+st.write(f"绿色产业目录 **{std.to_string(index=False)}**")
+st.write(f"绿色债券支持项目目录 **{std.to_string(index=False)}**")
+st.markdown("***")
+nrow=len(reports[reports["GL_code"]==std.to_string(index=False)])
+st.write(f"该项目共有 {nrow} 条记录")
 st.markdown("""对应报告如下：""")
+data_selected=reports[["ID","report_name","type","GL_name"]][reports["GL_code"]==std.to_string(index=False)]
+title=("债券代码","报告标题","报告类型","参考准则")
 
-
+if len(data_selected)>0:
+    fig = go.Figure(data=[go.Table(
+        columnorder=[1,2,3,4],
+        columnwidth=[150,900,200,350],
+        header=dict(values=title,
+                fill_color='rgba(79,162,134,0.3)',
+                align='left'),
+        cells=dict(values=[data_selected.ID, data_selected.report_name, data_selected.type,data_selected.GL_name],
+               fill_color='rgba(217,160,72,0.6)',
+               align='left'))
+]   )
+    fig.update_layout(
+        margin=dict(l=0, r=20, t=20, b=20)
+    )   
+    st.write(fig)
+else:
+    st.info("报告库内当前无合适报告")
