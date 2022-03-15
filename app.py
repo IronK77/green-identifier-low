9bc1f2177b57427632b90892f2b1272e009088f0
IronK77 <53202149+IronK77@users.noreply.github.com>
2022-03-15 10:56:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ Created on Sun Mar 13 22:52:13 2022
 @author: Administrator
 """
    
+
 import streamlit as st
 import numpy as np
 import pandas as pd
 from datetime import date
+import os
 
 
 today = date.today()
@@ -19,25 +21,28 @@ st.set_page_config(
     initial_sidebar_state='auto',
 )
 
-########################################################
-main_selectbox = st.sidebar.selectbox('查询项目',
-    ('Apple', 'Orange', 'Banana')
-)
 
+#def get_data(url):
+    #products_gl = 'https://github.com/IronK77/green-identifier/blob/f227f60d7fcb21a5a7b0e2992daa61ecd89ec6fa/product-guideline.csv'
+    #reports_gl = 'https://github.com/IronK77/green-identifier/blob/f227f60d7fcb21a5a7b0e2992daa61ecd89ec6fa/report-guideline.csv'
+#    reports = pd.read_csv(url)
+#    return reports
+
+########################################################
+products = pd.read_csv("product-guideline.csv",encoding='gbk')
+p_name=products.name_product.unique().tolist()
+    
+main_product=st.sidebar.selectbox('查询项目',p_name)
 st.sidebar.markdown("***")
 st.sidebar.markdown("### 遵循准则 ")
-sidebar_selection_1 = st.sidebar.checkbox(
-    '绿色产业目录',
-)
+sidebar_selection_1 = st.sidebar. checkbox(
+    '绿色产业目录',)
 sidebar_selection_2 = st.sidebar.checkbox(
-    '绿色债券支持项目目录',
-)
+    '绿色债券支持项目目录',)
 sidebar_selection_3 = st.sidebar.checkbox(
-    '节能环保清洁产业统计分类',
-)
+    '节能环保清洁产业统计分类',)
 sidebar_selection_4 = st.sidebar.checkbox(
-    '国民经济行业',
-)
+    '国民经济行业',)
 st.sidebar.markdown("***")
 sidebar_slider = st.sidebar.slider("报告显示数量",
     1, 10, (5)
@@ -46,11 +51,14 @@ sidebar_slider = st.sidebar.slider("报告显示数量",
 st.sidebar.markdown("***")
 st.sidebar.button('查询')
 #######################################################
+reports = pd.read_csv("report-guideline.csv",encoding='gbk')
 
 t1, t2 = st.columns(2)
 with t1:
     st.header('绿色债券识别')
-
+    st.markdown("""
+        通过输入产品、债券名称，得到相关准则信息
+    """)
 with t2:
     st.write("")
     st.write("")
@@ -59,11 +67,10 @@ with t2:
     """)
 
 st.write("")
-st.markdown("""
-通过输入产品、债券名称，得到相关准则信息
-""")
 st.markdown("***")
 length=""
+st.write(f"目前选择的产品/项目是 **{main_product}**")
+
 st.write('该项目共有',length,'条记录')
 st.markdown("***")
 st.markdown("""对应报告如下：""")
